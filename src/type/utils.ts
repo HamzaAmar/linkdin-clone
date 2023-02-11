c52ae0c2777763fc10db4ca05fab0210652b7831
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-02-11 13:14:46 +0100

    feat: add global type

---
+++ src/type/utils.ts
@@ -0,0 +1,18 @@
+export type Color =
+  | 'primary'
+  | 'indigo'
+  | 'danger'
+  | 'warning'
+  | 'success'
+  | 'yellow'
+  | 'mint'
+  | 'slate'
+  | 'purple'
+  | 'red'
+  | 'green'
+  | 'orange'
+
+export type Size = '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+export type Variant = 'solid' | 'outline' | 'soft' | 'transparent'
+export type Corner = 'circle' | 'sharp' | 'sm' | 'md' | 'lg'
+export type Gap = Size
