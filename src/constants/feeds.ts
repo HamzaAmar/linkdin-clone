b435b141d1713399c016681d47544c80b0999559
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2023-02-11 13:06:24 +0100

    feat: add constance section

---
+++ src/constants/feeds.ts
@@ -0,0 +1,23 @@
+export const FEEDS = [
+  {
+    slug: 'angular',
+    name: 'Angular js',
+    description: 'Company • Computer Software',
+    avatar:
+      'https://media-exp1.licdn.com/dms/image/C4D0BAQGsBuRuIbcD_w/company-logo_100_100/0/1620494673092?e=1672272000&v=beta&t=1ST8z1i1_0z0DKbgH04i-S-f7Zq9RonewFE22TjKuBA',
+  },
+  {
+    slug: 'jsworld-conferences',
+    name: 'JSWORLD Conferences',
+    description: 'Stay uptodate with the latest news on educational events organised by JSWOWLRD Conference.',
+    avatar:
+      'https://media-exp1.licdn.com/dms/image/C4D12AQHU5lKg6vmM7Q/series-logo_image-shrink_100_100/0/1646825650167?e=1669852800&v=beta&t=YTHIVYRlXO2FNEYaigM0joiE5-gOLx1a9THeCpIBy_Q',
+  },
+  {
+    slug: 'vercel',
+    name: 'Vercel',
+    description: 'Computer Software',
+    avatar:
+      'https://media-exp1.licdn.com/dms/image/C560BAQHzRmUo-uRc3g/company-logo_100_100/0/1587463448826?e=1672272000&v=beta&t=Y3PnXWFpheD_rxLiF2kffytDkUDPh8CkCups7EtT1KM',
+  },
+]
---
+++ src/constants/users.ts
@@ -0,0 +1,75 @@
+export const USER1 = [
+  {
+    slug: '124klsxdf',
+    name: 'Hamza MIloud amar',
+    avatar: 'https://picsum.photos/id/214/120/120',
+    description: 'Jam stack developer',
+    isFriend: true,
+  },
+  {
+    slug: '124klsxdsdff',
+    name: 'Karim Hello',
+    avatar: 'https://picsum.photos/id/127/120/120',
+    description: 'Web developer',
+    isFriend: false,
+  },
+  {
+    slug: '124klssdxdf',
+    name: 'Latifa S9ali',
+    avatar: 'https://picsum.photos/id/125/120/120',
+    description: 'React developer',
+    isFriend: true,
+  },
+  {
+    slug: '124klsxdsdfdfsff',
+    name: 'Hamza MIloud amar',
+    avatar: 'https://picsum.photos/id/123/120/120',
+    description: 'Full Stack developer',
+    isFriend: false,
+  },
+  {
+    slug: '124klsssdfsddxdf',
+    name: 'Lamia MIloud amar',
+    avatar: 'https://picsum.photos/id/124/120/120',
+    description: 'Front end developer',
+    isFriend: true,
+  },
+]
+
+export const USER2 = [
+  {
+    slug: '124klsxsddf',
+    name: 'Hamza MIloud amar',
+    avatar: 'https://picsum.photos/id/213/120/120',
+    description: 'Jam stack developer',
+    isFriend: false,
+  },
+  {
+    slug: '124klsxdsdff',
+    name: 'Karim Hello',
+    avatar: 'https://picsum.photos/id/123/120/120',
+    description: 'Web developer',
+    isFriend: true,
+  },
+  {
+    slug: '124klssdxdf',
+    name: 'Latifa S9ali',
+    avatar: 'https://picsum.photos/id/21/120/120',
+    description: 'React developer',
+    isFriend: true,
+  },
+  {
+    slug: '124klsxdsdfdfsff',
+    name: 'Hamza MIloud amar',
+    avatar: 'https://picsum.photos/id/45/120/120',
+    description: 'Full Stack developer',
+    isFriend: false,
+  },
+  {
+    slug: '124klsssdfsddxdf',
+    name: 'Lamia MIloud amar',
+    avatar: 'https://picsum.photos/id/65/120/120',
+    description: 'Front end developer',
+    isFriend: false,
+  },
+]
